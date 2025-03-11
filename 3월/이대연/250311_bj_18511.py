de970c702e226ace1f7bb71da58d24214bc02da3
daeyeonlee <angellee98@naver.com>
2025-03-11 17:48:21 +0900

    250311 Leedaeyeon

--- 3월/이대연/250311_bj_18511.py
+++ 3월/이대연/250311_bj_18511.py
@@ -2,9 +2,18 @@
 import sys
 sys.stdin = open("input.txt","r")
 
-N, K = int(input().split())     # N, K 입력받기
-elements = list(input().split())    # K의 원소들 입력받기
 
-max_num = 0    # 최대값을 구하기 위한 변수
-temp = []      # 숫자를 string으로 받아서 리스트에 넣은 뒤 합치기 위해 빈 리스트 생성
+
+
+N, K = map(int,input().split())    # N, K 입력받기
+elements = list(map(int,input().split()))   # K의 원소들 입력받기
+
+elements.sort(reverse=True) # 원소들 오름차순
+numbers = [] # 숫자 N을 문자열로 바꿔서 한 글자씩 numbers 리스트에 추가
+for i in range(len(str(N))):
+    numbers.append(int(str(N)[i]))
+print(elements)
+print(numbers)
+
+min_v =
 
--- 3월/이대연/250311_bj_28075.py
+++ 3월/이대연/250311_bj_28075.py
@@ -1,11 +1,31 @@
-N, M = map(int,input().split())
+# 28075_스파이
+
+##
+import sys
+sys.stdin = open("input.txt","r")
+
+N, M = map(int,input().split()) # N : 총 일수, M : 최소 기여도
 work = [list(map(int,input().split())) for _ in range(2)]
+visited = [[0] * 3 for _ in range(2)]
 
-print(work)
+result = 0
+for r in range(2):
+    for c in range(3):
+        score = work[r][c]
 
-score = 0
-
-def working():
-    global score
-    
-    pass
+def dfs(day,score,prev):
+    global result
+    if day == N:
+        if score >= M:
+            result += 1
+        return
+    for i in range(2):
+        for j in range(3):
+            next_score = score
+            if j == prev:
+                next_score += work[i][j] / 2
+            else:
+                next_score += work[i][j]
+            dfs(day+1, score, j)
+dfs(0,0,-1)
+print(result)
