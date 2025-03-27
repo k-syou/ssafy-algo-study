6405c3b27844d238e5ef8ed5b202bb657b66b4f8
LeeJiwoo <easywoo980902@gmail.com>
2025-03-27 17:59:32 +0900

    0327

---
+++ 3월/이대연/250325_bj_2748.py
@@ -0,0 +1,31 @@
+반복문으로 풀어보기
+
+n = int(input())
+dp = [0] * (n+1)
+
+dp[0] = 0
+dp[1] = 1
+
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
+
+# 재귀 함수로 풀어보기 (시간초과)
+
+# def fibo(x):
+#     global d
+#     if x < 2:
+#         d[x] = x
+#         return d[x]
+#     d[x] = fibo(x-1) + fibo(x-2)
+#     return d[x]
+#
+# n = int(input())
+# d = [0] * (n+1)
+#
+# fibo(n)
+# print(d[n])
+#
+
+
